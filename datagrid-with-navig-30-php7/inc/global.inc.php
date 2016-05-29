da8155c8c71e7e34bb38929bc73f55b3b6ababf1
Sérgio Soares <serguiomicaelo@gmail.com>
2016-05-29 14:50:26 +0100

    Update global.inc.php

--- datagrid-with-navig-30-php7/inc/global.inc.php
+++ datagrid-with-navig-30-php7/inc/global.inc.php
@@ -1,7 +1,7 @@
 <?php
 $host = '127.0.0.1';
 $user = 'root';
-$pass = '321sergio321';
-$name = 'datagrid';
+$pass = 'yourdbpassword';
+$name = 'yourdbname'; 
 $DB = new BasicMySQL($host,$user,$pass,$name);
 ?>
